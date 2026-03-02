1da0fda9898c77bd2125073c9a4abc4694802407
tyburski-stack <matthewtyburski2007@gmail.com>
2026-03-02 09:57:01 -0500

    Revert back to label exec block

--- simple-isa/Syntax.v
+++ simple-isa/Syntax.v
@@ -1,6 +1,8 @@
 
+
 Set Warnings "-notation-overridden".
 From Stdlib Require Import Arith Nat Bool Lists.List.
+
 
 
 Definition reg  := nat.
@@ -30,37 +32,38 @@ Definition tv_taint (x : tval) : taint := snd x.
 Definition mk_tval (v : val) (t : taint) : tval := (v, t).
 
 
-(* program counter identifies instruction location inside program **)
-Record pc : Type := {
-  pc_lbl : label; (* which block **)
-  pc_ix  : nat;   (* which instr in block **)
-}.
-
 
 (* Extend induction Syntax ... *)
 Inductive size : Type := S1 | S2 | S4 | S8.
 
-(* edit instr (straight-line + ctrl flow) **)
 Inductive instr : Type :=
 | Nop : instr
 | Add : reg -> reg -> reg -> instr                 (* rd rs1 rs2 *)
 | Mul : reg -> reg -> reg -> instr                 (* rd rs1 rs2 *)
 | Load  : size -> reg -> reg -> nat -> instr       (* sz rd base off *)
-| Store : size -> reg -> reg -> nat -> instr       (* sz rs base off *)
-| Jmp   : label -> instr
-| BrZero : reg -> label -> instr
-| Halt  : instr. 
+| Store : size -> reg -> reg -> nat -> instr.      (* sz rs base off *)
 
 
-(* Update block **)
-Definition block : Type := list instr.
+(* Transition from BrZero to separate terminator type*)
+Inductive term : Type :=
+| TJmp : label -> term
+| TBrZero : reg -> label -> label -> term
+| THalt : term.
 
-Definition prog := label -> option block. (* map labels to blocks *)
+
+(* Block record *)
+Record block : Type := {
+  code : list instr;
+  termi: term;
+}.
+
+
+Definition prog := label -> option block. (* Update from option instr to option block*)
 Definition regs := reg -> tval. (* (val * taint) *)
 Definition mem  := addr -> option tbyte.
 
 Record state : Type := {
-  pcv : pc;
+  pc : label;
   rf : regs;
   mm : mem;
 }.
@@ -71,5 +74,3 @@ Definition regs_set (r : regs) (x : reg) (tv : tval) : regs :=
 (* updated  mem_set to write t_byte*)
 Definition mem_set (m : mem) (a : addr) (tb : tbyte) : mem :=
   fun b => if Nat.eqb b a then Some tb else m b.
-
-
